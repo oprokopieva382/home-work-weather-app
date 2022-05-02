9e38d125f3f9c58f3774cf8a97b69e0670a936f5
oprokopieva382 <oprokopieva382@gmail.com>
2022-05-02 11:41:55 -0500

    added js real temp and date

---
+++ src/app.js
@@ -0,0 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(responce) {
+  console.log(responce.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = responce.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(responce.data.main.temp);
+  descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = responce.data.weather[0].description;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(responce.data.dt * 1000);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = responce.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(responce.data.wind.speed);
+}
+
+let apiKey = "31062848022c12a9b5c54447286aa8b2";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
