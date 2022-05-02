f4a4a7e89b303765b99f3fe97630a18350014614
oprokopieva382 <oprokopieva382@gmail.com>
2022-05-02 16:12:29 -0500

    added search form

--- src/app.js
+++ src/app.js
@@ -42,8 +42,19 @@ function displayTemperature(responce) {
   );
   iconElement.setAttribute("alt", responce.data.weather[0].icon);
 }
+function search(city) {
+  let apiKey = "31062848022c12a9b5c54447286aa8b2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "31062848022c12a9b5c54447286aa8b2";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("new York");
